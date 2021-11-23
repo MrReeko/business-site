b3690990b909805382de8982d9bb25a0e1caf943
MrReeko <misc@adarastgermbp13.lan>
2021-11-22 23:55:41 -0800

    logs

--- models/gmailAuth.js
+++ models/gmailAuth.js
@@ -7,6 +7,9 @@ const SCOPES = ['https://www.googleapis.com/auth/gmail.send'];
 const TOKEN_PATH = __dirname + '/token.json';
 
 function authorize(credentials) {
+    console.log('received credentials: ')
+    console.log(typeof credentials);
+    console.log(credentials);
     const {client_secret, client_id, redirect_uris} = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(
         client_id, client_secret, redirect_uris[0]);
