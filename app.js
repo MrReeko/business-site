d6545e6877ac797cdde887f7a84a56642390b314
MrReeko <misc@adarastgermbp13.lan>
2021-11-22 20:21:28 -0800

    initial version of therapy site

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const port = 3000;
 const express = require('express');
 var flash = require('connect-flash');
@@ -8,7 +9,7 @@ const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(cookieParser());
-app.use(session({secret: 'keyboard cat', resave: true, saveUninitialized: true, cookie: { maxAge: 60000}}));
+app.use(session({secret: process.env.sessionSecret, resave: true, saveUninitialized: true, cookie: { maxAge: 60000}}));
 app.use(flash());
 app.use(express.json());
 app.use(express.urlencoded({
